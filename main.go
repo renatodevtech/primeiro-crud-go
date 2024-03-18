6c88904e84dd584ba1ec6b90e59f75148fa94cbe
renatodevtech <renatodevtech@gmail.com>
2024-03-18 14:23:05 -0300

    inclusao do arquivo .env

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loadind .env file")
+	}
+	fmt.Println(os.Getenv("TEST"))
+
+}
